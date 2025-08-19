3f0f9931e5a251d4633c80b922d74bf49e1e29c0
gulcin <gulcin_375@hotmail.com>
2025-08-19 22:41:04 +0300

    feat: use PORT from .env and fix error handling in deleteProduct

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,11 @@ const app = express();
 
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server started at http://localhost:${PORT}`);
 });
