3828227f44271672fdc45df4f11e332187a9f879
gulcin <gulcin_375@hotmail.com>
2025-08-19 18:37:08 +0300

    feat: added get and delete API

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching the data", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = req.body;
 
@@ -25,6 +35,17 @@ app.post("/api/products", async (req, res) => {
   } catch (error) {
     console.error("Error in creating the product", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted." });
+  } catch (error) {
+    console.log("Error in deleting product.", error.message);
+    res.status(400).json({ success: false, message: "Product not found." });
   }
 });
 
